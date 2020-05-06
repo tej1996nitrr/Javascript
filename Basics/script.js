b29990e51500697815bd414120d5a3c9d30e8ec9
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-06 21:58:32 +0530

    arrays

--- Basics/script.js
+++ Basics/script.js
@@ -135,3 +135,64 @@ function calculateAge(birthyear) {
 var ageJohn = calculateAge(1998)
 var ageMark = calculateAge(1997)
 console.log(ageJohn,ageMark)
+//=====================================Arrays=============================================
+var names = ['John','Mark','Jane']
+var years = new Array(1999,2000,2001)
+console.log(names[2])
+console.log(names[2]);
+console.log(names.length);
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+console.log(john.indexOf(23));
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. 
+The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator
+ (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when
+  the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+*/
+function tipCalculator(bill) {
+ var tip=0
+ if(bill<50)
+ {
+     tip=bill*0.2
+ }
+ else if(bill>=50 && bill<=200)
+ {
+     tip=bill*0.15
+ }
+ else{
+     tip=bill*0.1
+ }
+  return tip
+}
+
+var bills = [124, 48, 268];
+var tips=[]
+var paid=[]
+for(j in bills)
+{
+console.log(bills[j])
+tips.push(tipCalculator(bills[j]))
+paid.push(bills[j]+tips[j])
+}
+console.log(tips)
+console.log(paid)
