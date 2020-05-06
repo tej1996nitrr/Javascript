e778dd8586f5a46843fb8a3ee15dab8b58e9ec98
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-06 21:56:42 +0530

    variable mutation

--- Basics/script.js
+++ Basics/script.js
@@ -16,3 +16,14 @@ console.log(job)
 
 job="teacher"
 console.log(job)
+//====================================Variables Mutation and type coercion==============================================
+
+console.log(firstname+" "+age)
+job="finance"
+var isMarried =  false
+console.log(firstname+' is in '+ job+'. Is (s)he married?'+isMarried)
+age="twenty eight"
+job="driver"
+console.log(firstname+' is a '+ age+' year old '+job+'. Is (s)he married?'+isMarried)
+// var lastname=prompt("What is the last name?")
+// console.log(firstname+" "+lastname)
