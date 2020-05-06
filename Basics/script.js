c881ff6a9e59b7963aa68fb43ad20612f098da06
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-06 21:56:16 +0530

    variables

---
+++ Basics/script.js
@@ -0,0 +1,18 @@
+//====================================Variables and datatypes==============================================
+var firstname='John'
+console.log(firstname)
+
+var lastname = 'Smith'
+console.log(lastname)
+
+var age =30
+console.log(age)
+
+var qualified=true
+console.log(qualified)
+
+var job
+console.log(job)
+
+job="teacher"
+console.log(job)
