6847de12c6e771ebf4249edfc6f7e5f4ffe1eb47
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-06 21:57:51 +0530

    conditionals

--- Basics/script.js
+++ Basics/script.js
@@ -49,3 +49,81 @@ console.log(typeof(johnOlder))
 console.log(typeof(age))
 var x
 console.log(typeof(x))
+//=====================================Conditionals=============================================
+
+var firstname="John"
+var civilStatus="single"
+if (civilStatus==="married")
+{
+    console.log(firstname+" is married")
+}
+else{
+    console.log(firstname+" is not married")
+}
+isMarried=true
+if (isMarried)
+{
+    console.log(firstname+" is married")
+}
+else{
+    console.log(firstname+" is not married")
+}
+var massMark=70
+var heightMark=1.7
+var massJohn=60
+var heightJohn=1.3
+var BMIMark = massMark/(heightMark*heightMark)
+var BMIJohn = massJohn/(heightJohn*heightJohn)
+if(BMIJohn>BMIMark)
+{
+    console.log("John > Mark BMI")
+}
+else{
+    console.log("Mark > John BMI")
+}
+//Boolean logic
+var age=30
+if (age<13)
+{
+    console.log("Child")
+}
+else if(age>=13 && age <20)
+{
+    console.log("Teenager")
+
+}
+else if(age>=20 && age <30)
+{
+    console.log("Young man")
+
+}
+else if(age>=30 && age <40)
+{
+    console.log("Man")
+
+}
+else if(age>=40)
+{
+    console.log("Old Man")
+
+}
+//switch case
+var job = "instructor"
+switch(job)
+{
+    case 'teacher':
+    case 'instructor':
+        console.log(firstname+" teaches/instructs")
+        break
+    case 'driver':
+        console.log(firstname+" drives")
+        break
+
+    case 'designs':
+        console.log(firstname+" designs")
+        break
+
+    default:
+        console.log(firstname+" does something else")
+
+}
