2823b193300fd66b8549411f136661c2298ee2ca
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-10 20:49:23 +0530

    querySelector

--- DOM/dom.js
+++ DOM/dom.js
@@ -35,13 +35,31 @@
 //     items[i].style.backgroundColor="grey"
 // }
 //--------------------------------getElementByTagName=----------------------------------
-var items = document.getElementsByTagName('li')
-console.log(items)
-console.log(items[1])
-items[1].textContent="Hello 3"
-items[1].style.fontWeight='bold'
-items[0].style.backgroundColor="red"
-for(var i=0; i<items.length;i++)
-{
-    items[i].style.backgroundColor="grey"
-}
+// var items = document.getElementsByTagName('li')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent="Hello 3"
+// items[1].style.fontWeight='bold'
+// items[0].style.backgroundColor="red"
+// for(var i=0; i<items.length;i++)
+// {
+//     items[i].style.backgroundColor="grey"
+// }
+//--------------------------------querySelector----------------------------------------------
+var header  = document.querySelector('#main-header')
+header.style.borderBottom='solid 3px #000'
+
+var input = document.querySelector('input')
+input.value="Hello World"
+var submit=document.querySelector('input[type="submit"]')
+submit.value="SEND"
+
+var item =document.querySelector('.list-group-item')
+item.style.color="red"
+
+var last_item =document.querySelector('.list-group-item:last-child') 
+last_item.style.color="green"
+
+var second_item = document.querySelector('.list-group-item:nth-child(2)')
+second_item.style.color="blue"
+
