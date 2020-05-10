bbc6a9965b3b7c90c9298e0899b05a8a06c4b202
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-10 20:37:42 +0530

    getElementByCLassName

--- DOM/dom.js
+++ DOM/dom.js
@@ -13,14 +13,24 @@
 // console.log(document.images)
 
 //------------------------------------getElementbyID-------------------------------------------
-console.log(document.getElementById('header-title'))
-var headerTitle = document.getElementById('header-title')
-var header = document.getElementById('main-header')
-console.log(headerTitle)
-console.log(headerTitle.textContent="Hello")
-headerTitle.innerText="Bye"
-headerTitle.innerHTML='<h3> Hello what</h3>'
-headerTitle.style.borderBottom='solid 3px #000'
-header.style.borderBottom='solid 3px #000'
+// console.log(document.getElementById('header-title'))
+// var headerTitle = document.getElementById('header-title')
+// var header = document.getElementById('main-header')
+// console.log(headerTitle)
+// console.log(headerTitle.textContent="Hello")
+// headerTitle.innerText="Bye"
+// headerTitle.innerHTML='<h3> Hello what</h3>'
+// headerTitle.style.borderBottom='solid 3px #000'
+// header.style.borderBottom='solid 3px #000'
 
 //--------------------------------getElementByCLassName=----------------------------------
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[1].textContent="Hello 3"
+items[1].style.fontWeight='bold'
+items[0].style.backgroundColor="red"
+for(var i=0; i<items.length;i++)
+{
+    items[i].style.backgroundColor="grey"
+}
