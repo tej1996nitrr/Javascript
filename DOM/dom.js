95c74352efd7d1106a238e767301364b57a0424a
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-10 20:40:06 +0530

    getElementByTagName

--- DOM/dom.js
+++ DOM/dom.js
@@ -24,7 +24,18 @@
 // header.style.borderBottom='solid 3px #000'
 
 //--------------------------------getElementByCLassName=----------------------------------
-var items = document.getElementsByClassName('list-group-item')
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent="Hello 3"
+// items[1].style.fontWeight='bold'
+// items[0].style.backgroundColor="red"
+// for(var i=0; i<items.length;i++)
+// {
+//     items[i].style.backgroundColor="grey"
+// }
+//--------------------------------getElementByTagName=----------------------------------
+var items = document.getElementsByTagName('li')
 console.log(items)
 console.log(items[1])
 items[1].textContent="Hello 3"
