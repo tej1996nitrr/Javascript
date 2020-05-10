117c8723ea9318bc5bafd354225dce9280b75ece
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-10 19:39:04 +0530

    document

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,1 +1,9 @@
-console.dir(document)
+// console.dir(document)
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+console.log(document.doctype)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+document.all[10].textContent="Hello"
